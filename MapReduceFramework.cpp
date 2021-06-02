6efa8ae1edc4eeab99c8871e5217b40f9a82a41b
idan.stollman <idan.stollman@mail.huji.ac.il>
2021-06-02 21:24:57 +0300

    working on init jobManager

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -4,6 +4,57 @@
 
 #include "MapReduceFramework.h"
 
+void *threadJob(void *arg);
+
+int spawnThreads(JobManager *jobManager);
+
+int initJobManager(JobManager *jobManager, int threadsNum);
+
+void emit2(K2 *key, V2 *value, void *context) {}
+
+void emit3(K3 *key, V3 *value, void *context) {}
+
+JobHandle startMapReduceJob(const MapReduceClient &client,
+                            const InputVec &inputVec, OutputVec &outputVec,
+                            int multiThreadLevel) {
+    auto *jobManager = new JobManager();
+    initJobManager(jobManager, multiThreadLevel);
+    spawnThreads(jobManager);
+
+
+
+    return jobManager;
+}
+
+void waitForJob(JobHandle job) {}
+
+
+void getJobState(JobHandle job, JobState *state) {}
+
+void closeJobHandle(JobHandle job) {}
+
+// ______________________________________________________________________
+
+
+void *threadJob(void *arg) {
+
+}
+
+int initJobManager(JobManager *jobManager, int threadsNum) {
+    jobManager->barrier = new Barrier(threadsNum);
+    jobManager->ThreadsNum = threadsNum;
+    jobManager->threads = new pthread_t[threadsNum];
+    jobManager->threadsContexts = new ThreadContext[threadsNum];
+
+}
+
+int spawnThreads(JobManager *jobManager) {
+    for (int i = 0; i < jobManager->ThreadsNum; ++i) {
+        pthread_create((jobManager->threads) + i, NULL, threadJob, (jobManager->threadsContexts) + i);
+    }
+
+}
+
 
 
 
